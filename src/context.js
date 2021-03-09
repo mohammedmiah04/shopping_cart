281b34b5fc5711e975479acbfeefa35094cc240b
MohammedM <nanumiah@nanus-mbp.home>
2021-03-09 15:57:53 +0000

    basic html/css setup dpne + aded clear func

---
+++ src/context.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-useReducer-cart-project";
+const AppContext = React.createContext();
+
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+};
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
